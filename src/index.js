6784422f980c4e9304d7e9186cfb0a27cacddeff
perade <perade0530@gmail.com>
2020-03-18 00:08:35 +0900

    1.2 Actions 실습

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const reducer = (state, action) => {
-  return state;
+const reducer = (state = 0, action) => {
+  if (action.type === 'ADD') {
+    return state + 1;
+  } else if (action.type === 'MINUS') {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
 const store = createStore(reducer);
+
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'ADD' });
+store.dispatch({ type: 'MINUS' });
 
 console.log(store.getState());

fe278a110b5f4f7fa3fc174b3afb7b44ec93946d
perade <perade0530@gmail.com>
2020-03-22 23:42:22 +0900

    2.0 Vanilla ToDo 실습

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
 import { createStore } from 'redux';
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
-number.innerText = 0;
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-const ADD = 'ADD';
-const MINUS = 'MINUS'
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
 
-const reducer = (state = 0, action) => {
-  switch(action.type) {
-    case ADD:
-      return state + 1;
-    case MINUS:
-      return state - 1;
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
       return state;
   }
 };
+
 const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = store.getState();
-};
-store.subscribe(onChange);
-
-const handleAdd = () => {
-  store.dispatch({ type: ADD });
-};
-const handleMinus = () => {
-  store.dispatch({ type: MINUS });
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+form.addEventListener('submit', onSubmit);

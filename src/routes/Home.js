c3ef20e9274e7a131ce36b99d4213f2626f7b301
perade <perade0530@gmail.com>
2020-03-31 01:53:40 +0900

    3.4 Deleting To Do 실습

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import ToDo from '../components/ToDo';
 
 function Home ({ toDos, addToDo }) {
   const [text, setText] = useState('');
@@ -20,7 +21,7 @@ function Home ({ toDos, addToDo }) {
         <button>Add</button>
       </form>
       <ul>
-        {JSON.stringify(toDos)}
+        {toDos.map(toDo => <ToDo {...toDo} key={toDo.id} />)}
       </ul>
     </>
   );

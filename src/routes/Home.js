a70e0bd71ea6dece0f9069b96a409f724fd293f7
perade <perade0530@gmail.com>
2020-03-31 01:39:08 +0900

    3.3 mapDispatchToProps 실습

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { actionCreators } from '../store';
 
-function Home ({ toDos }) {
+function Home ({ toDos, addToDo }) {
   const [text, setText] = useState('');
   function onChange (e) {
     setText(e.target.value);
   };
   function onSubmit (e) {
     e.preventDefault();
+    addToDo(text);
     setText('');
   };
   return (
@@ -28,5 +30,15 @@ function mapStateToProps (state) {
   return { toDos: state };
 };
 
-// Home을 store에 연결 후, mapStateToProps를 활용하여 Home의 props에다 state를 전달
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps (dispatch) {
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
+};
+
+/**
+ * Home을 store에 연결 후,
+ * mapStateToProps를 활용하여 Home의 props에다 state를 전달
+ * mapDispatchToProps를 활용하여 Home의 props에다 dispatch를 전달
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
